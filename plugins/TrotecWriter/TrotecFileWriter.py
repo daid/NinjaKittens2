c0e50a401fc38f56076a86fe2c58af3e1168c5ea
daid <daid303@gmail.com>
2016-04-25 17:26:18 +0200

    Update code for new jobcontrol.

--- plugins/TrotecWriter/TrotecFileWriter.py
+++ plugins/TrotecWriter/TrotecFileWriter.py
@@ -12,32 +12,12 @@ from nk import Paths
 class TrotecFileWriter(MeshWriter):
     def __init__(self, filename):
         super().__init__()
-        self._bmp_magic = b"""eJztyTEKwkAURdFvJzbBHVimTG3CIAMpg4UuwiaQFWTlwjgDVu5AOOdxqzc9nnndrpdhHFJe9tc5
-mlTra3sX8T5GHOqa2/f/VUppAQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAPCHpvme1y2dPr1q6Hs="""
-        self._dpi = 500.0
+        self._dpi = 600.0
         self._max_y = None
         self._max_x = None
         self._min_y = None
         self._min_x = None
+        self._family_id = 0
         self._filename = filename
 
     def write(self, stream, node, mode=MeshWriter.OutputMode.BinaryMode):
@@ -57,6 +37,7 @@ AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAPCHpvme1y2dPr1q6Hs="""
         self._max_x = -10000000000
         self._min_y = 10000000000
         self._max_y = -10000000000
+        self._family_id = 0
         for path in engrave_paths.open_paths:
             self._processPathMinMax(path)
         for path in engrave_paths.closed_paths:
@@ -66,42 +47,57 @@ AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAPCHpvme1y2dPr1q6Hs="""
         for path in cut_paths.closed_paths:
             self._processPathMinMax(path)
 
-        stream.write(b'<!-- Version: 9.4.2.1034>\r\n')
-        stream.write(b'<!-- PrintingApplication: ps2tsf.exe>\r\n')
-        stream.write(b'<BegGroup: Header>\r\n')
-        stream.write(b'<ProcessMode: Standard>\r\n')
-        stream.write(('<Size: %.2f;%.2f>\r\n' % (self._max_x - self._min_x + 1.0, self._max_y - self._min_y + 1.0)).encode('utf-8'))
-        stream.write(b'<MaterialGroup: Standard>\r\n')
-        stream.write(b'<MaterialName: 0.0 mm>\r\n')
-        stream.write(('<JobName: %s>\r\n' % (os.path.basename(self._filename))).encode('utf-8'))
-        stream.write(b'<JobNumber: 1>\r\n')
-        stream.write(('<Resolution: %i>\r\n' % (self._dpi)).encode('utf-8'))
-        stream.write(b'<Cutline: none>\r\n')
-        stream.write(b'<EndGroup: Header>\r\n')
-        stream.write(b'<BegGroup: Bitmap>\r\n')
-        stream.write(zlib.decompress(base64.decodebytes(self._bmp_magic)))
-        stream.write(b'<EndGroup: Bitmap>\r\n')
-        stream.write(b'<BegGroup: DrawCommands>\r\n')
-
+        self._writeTag(stream, "!-- Version:Driver 10.5.0.168")
+        self._writeTag(stream, "!-- Version:Bezier 10.5.0.168")
+        self._writeTag(stream, "!-- PrintingApplication", "unidrive.exe")
+        self._writeTag(stream, "BegGroup", "Header")
+        self._writeTag(stream, "Version", "1.7")
+        self._writeTag(stream, "Speedy")
+        self._writeTag(stream, "ProcessMode", "Standard")
+        self._writeTag(stream, "Resolution", "%d" % (self._dpi))
+        self._writeTag(stream, "Cutline", "none")
+        self._writeTag(stream, "Size.Orig", "%.2f;%.2f" % (self._max_x - self._min_x + 1.0, self._max_y - self._min_y + 1.0))
+        self._writeTag(stream, "Size", "%.2f;%.2f" % (self._max_x - self._min_x + 1.0, self._max_y - self._min_y + 1.0))
+        self._writeTag(stream, "MaterialGroup", "Standard")
+        self._writeTag(stream, "MaterialName", "Standard")
+        self._writeTag(stream, "JobName", "%s" % (os.path.basename(self._filename)))
+        self._writeTag(stream, "JobNumber", "1")
+        self._writeTag(stream, "AutoPosition")
+        self._writeTag(stream, "EndGroup", "Header")
+        self._writeTag(stream, "BegGroup", "JobMeta")
+        self._writeTag(stream, "Meta", "Halftoning; Color")
+        self._writeTag(stream, "EndGroup", "JobMeta")
+        self._writeTag(stream, "BegGroup", "DrawCommands")
         self._outputPaths(stream, engrave_paths, 255, 0, 0)
         self._outputPaths(stream, cut_paths, 0, 0, 255)
-
-        stream.write(b'<EndGroup: DrawCommands>\r\n')
+        self._writeTag(stream, "EndGroup", "DrawCommands")
 
         return True
 
+    def _writeTag(self, stream, key, value=None):
+        if value is not None:
+            stream.write(("<%s: %s>\r\n" % (key, value)).encode("utf-8"))
+        else:
+            stream.write(("<%s>\r\n" % (key)).encode("utf-8"))
+
     def _outputPaths(self, stream, paths, r, g, b):
         for poly in paths.open_paths:
+            self._writeTag(stream, "BegGroup", "Family_%d" % (self._family_id))
             stream.write(('<DrawPolygon: %i;%i;%i;%i' % (len(poly), r, g, b)).encode('utf-8'))
             for point in poly:
                 stream.write((';%i;%i' % self._convertPoint(point)).encode('utf-8'))
             stream.write(b'>\r\n')
+            self._writeTag(stream, "EndGroup", "Family_%d" % (self._family_id))
+            self._family_id += 1
         for poly in paths.closed_paths:
+            self._writeTag(stream, "BegGroup", "Family_%d" % (self._family_id))
             stream.write(('<DrawPolygon: %i;%i;%i;%i' % (len(poly) + 1, r, g, b)).encode('utf-8'))
             for point in poly:
                 stream.write((';%i;%i' % self._convertPoint(point)).encode('utf-8'))
             stream.write((';%i;%i' % self._convertPoint(poly[0])).encode('utf-8'))
             stream.write(b'>\r\n')
+            self._writeTag(stream, "EndGroup", "Family_%d" % (self._family_id))
+            self._family_id += 1
 
     def _convertPoint(self, point):
         return ((point[0] / Paths.SCALE) - self._min_x) / 25.4 * self._dpi, ((point[1] / Paths.SCALE) - self._min_y) / 25.4 * self._dpi
